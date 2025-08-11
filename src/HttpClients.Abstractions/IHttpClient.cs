3279ad31fc06914a82439a7b79a562d6aa1c7a85
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2025-08-11 17:16:16 -0300

    Add BaseAddress property.

--- src/HttpClients.Abstractions/IHttpClient.cs
+++ src/HttpClients.Abstractions/IHttpClient.cs
@@ -6,4 +6,10 @@
 /// Typically, implementations of this interface provide methods to perform HTTP operations
 /// such as GET, POST, PUT, DELETE, and PATCH.
 /// </summary>
-public interface IHttpClient;
+public interface IHttpClient
+{
+    /// <summary>
+    /// Gets the base address of the HTTP client used for requests.
+    /// </summary>
+    Uri? BaseAddress { get; }
+}
